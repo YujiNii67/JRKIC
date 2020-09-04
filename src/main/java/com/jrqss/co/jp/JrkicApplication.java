8d75a91455d448a3f05deedaad79c607443318dd
SS05020001 <SS05020001@SSHQA036.AD-JRKYU-G.local>
2020-09-04 09:06:38 +0900

    新規登録

---
+++ src/main/java/com/jrqss/co/jp/JrkicApplication.java
@@ -0,0 +1,13 @@
+package com.jrqss.co.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrkicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrkicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrqss/co/jp/JrkicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrqss.co.jp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrkicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
